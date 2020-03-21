f5598017d527bf6d9de15e9e30dec09314953b14
Martin Evald <martin.evald@gmail.com>
2020-03-21 19:06:20 +0100

    Included HTML-ified sourcecode in docs.

--- generate_apidocs.sh
+++ generate_apidocs.sh
@@ -2,6 +2,7 @@
 # vim: syntax=sh
 doxygen doxygen/Doxyfile
 cp doxygen/html/shutf8_8h.html docs/index.html
+cp doxygen/html/shutf8_8h_source.html docs/
 cp doxygen/html/*.png docs/
 cp doxygen/html/*.css docs/
 
