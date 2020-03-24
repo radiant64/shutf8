8c6a4523a2e15d9c64ff2a4c2706a03481071261
Martin Evald <martin.evald@gmail.com>
2020-03-24 21:34:02 +0100

    Test program now returns number of failed tests.

--- tests/test_shutf8.c
+++ tests/test_shutf8.c
@@ -5,27 +5,38 @@
 #include "test_step.h"
 
 int main(int argc, char** argv) {
+    int total = 0;
+    int successful = 0;
     {
         t_testsuite suite = prepare_decode_codepoint();
         run_tests(&suite);
+        total += suite.total;
+        successful += suite.successful;
     }
     {
         t_testsuite suite = prepare_encode_codepoint();
         run_tests(&suite);
+        total += suite.total;
+        successful += suite.successful;
     }
     {
         t_testsuite suite = prepare_decode_alloc();
         run_tests(&suite);
+        total += suite.total;
+        successful += suite.successful;
     }
     {
         t_testsuite suite = prepare_encode_alloc();
         run_tests(&suite);
+        total += suite.total;
+        successful += suite.successful;
     }
     {
         t_testsuite suite = prepare_step();
         run_tests(&suite);
+        total += suite.total;
+        successful += suite.successful;
     }
-    return 0;/*(suite.total - suite.successful);
-*/
+    return total - successful;
 }
 
